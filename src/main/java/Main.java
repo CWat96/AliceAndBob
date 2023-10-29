215a7325f8562bdb9d646a05d079858dab30f061
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-10-29 16:39:21 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
-    }
+        if (name.equals("Alice") || name.equals("Bob")) {
+        System.out.println("Hello, " + name + "!");
+
+        } else {
+            System.out.println("You are neither Alice nor Bob.");
+        }
+}
 }
